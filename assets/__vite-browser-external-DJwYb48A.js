15c3c405f37d58f52d104852fe832cd45d83c336
K <k.raskovic.6@gmail.com>
2026-05-25 20:25:41 +0400

    Updates

---
+++ assets/__vite-browser-external-DJwYb48A.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-CyZqivpU.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
--- assets/__vite-browser-external-Db9DYbmT.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-DdE3UTNV.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
