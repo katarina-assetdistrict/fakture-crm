00165c91f4873e358db6cb4bf428a0842f829f60
K <k.raskovic.6@gmail.com>
2026-05-25 19:24:16 +0400

    Updates

--- assets/__vite-browser-external-CLMKFYBl.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-CAlkhhJa.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
---
+++ assets/__vite-browser-external-DFt08IIy.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-BKwcRk5t.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
